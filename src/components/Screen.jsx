de0b3eeb96ceaa14a4fd0144e2097d1df30f5ec0
DESKTOP-CGNLPA7\lance <lancellotti.marco@yahoo.it>
2021-01-22 14:51:10 +0100

    Setup and Layout
    Setup and Layout

---
+++ src/components/Screen.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Loader from './Loader';
+
+const Screen = () => {
+  return (
+    <section id="screen">
+      <div id="pokedex"></div>
+      <Loader />
+    </section>
+  );
+};
+
+export default Screen;
