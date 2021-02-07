f5788f8d672bdc767b9ee4b6fa3e4a483633239c
DESKTOP-CGNLPA7\lance <lancellotti.marco@yahoo.it>
2021-02-07 17:21:31 +0100

    infinite scroling logics

--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -1,25 +1,40 @@
-import React, {useState, useEffect} from 'react';
+import React, {useRef, useState, useEffect, useCallback} from 'react';
 import Loader from './Loader';
 import axios from 'axios';
 import PokeCard from './PokeCard';
 
 
 const Screen = () => {
+  const [nextUrl, setNextUrl] = useState(null);
   const [pokemon, setPokemon] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  
+  const observer = useRef();
+  const lastElementRef = useCallback((node) => {
+    if(isLoading) return;
+    if(observer.current) observer.current.disconnect()
+    observer.current = new IntersectionObserver(entries => {
+      if(entries[0].isIntersecting && nextUrl){
+        searchPokedex(nextUrl)
+      }
+    });
+    if(node) observer.current.observe(node);
+    
+  }, [isLoading, nextUrl, pokemon]);
 
   const fetchPokemonDetail = async url => {
     const response = await axios.get(url);
-    console.log(response);
     const {name, id, types, sprites} = response.data;
     return {id, name, type: types[0].type.name, img: sprites.front_default}
   }
 
-  const searchPokedex = async url => {
+  const searchPokedex = useCallback(async url => {
     setIsLoading(true);
     try{
       const response = await axios.get(url);
       const results = response.data.results;
+      const {next} = response.data;
+      if(next) setNextUrl(next);
       const detailRequests = results.map(async r => await fetchPokemonDetail(r.url))
 
       await Promise.all(detailRequests).then(detailResults => {
@@ -32,14 +47,18 @@ const Screen = () => {
     } finally{
       setIsLoading(false)
     }
-  }
-
+  }, [nextUrl, pokemon])
+  
   useEffect(() => {
     const url = 'https://pokeapi.co/api/v2/pokemon/';
     searchPokedex(url)
   }, []);
 
-  const renderPokemon = () => pokemon.map(p => <div key={p.id}><PokeCard pokemon={p} /></div>);
+  const renderPokemon = () => pokemon.map((p, i) => {
+    return i === pokemon.length -1 
+    ? <div key={p.id} ref={lastElementRef}><PokeCard pokemon={p} /></div> 
+    : <div key={p.id}><PokeCard pokemon={p} /></div>
+});
 
   return (
     <section id="screen">
